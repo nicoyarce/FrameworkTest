6c03cef808454815bac3660fd4740876e71ed39b
nicoyarce <nicoyarce@gmail.com>
2017-10-09 16:54:07 -0300

    Dos variables, un objetivo

--- src/prueba/EjecutarProblema.java
+++ src/prueba/EjecutarProblema.java
@@ -1,6 +1,7 @@
 package prueba;
 
 import org.moeaframework.Executor;
+import org.moeaframework.analysis.plot.Plot;
 import org.moeaframework.core.NondominatedPopulation;
 import org.moeaframework.core.Solution;
 import org.moeaframework.core.variable.EncodingUtils;
@@ -13,16 +14,22 @@ public class EjecutarProblema {
 
     public static void main(String[] args) {
         NondominatedPopulation result = new Executor()
-                .withAlgorithm("NSGAII")
-                .withProblemClass(Funcion.class)        
-                .withMaxEvaluations(1000000000)
+                .withAlgorithm("NSGAII")  //algoritmo a utilzar
+                .withProblemClass(Funcion.class)  //clase donde esta el problema
+                .withMaxEvaluations(10000)    //cantidad de evaluaciones
                 .run();
 
         for (Solution solution : result) {
-            System.out.printf("%.5f => %.5f\n",
+            System.out.printf("%.5f => %.5f - %.5f => %.5f\n",
                     EncodingUtils.getReal(solution.getVariable(0)),
+                    solution.getObjective(0),
+                    EncodingUtils.getReal(solution.getVariable(1)),
                     solution.getObjective(0));
         }
+        //Para graficar frontera de pareto
+        /*new Plot()
+                .add("NSGAII",result)
+                .show();*/
 
     }
 }
--- src/prueba/Funcion.java
+++ src/prueba/Funcion.java
@@ -11,19 +11,26 @@ import org.moeaframework.problem.AbstractProblem;
 public class Funcion extends AbstractProblem {
 
     public Funcion() {
-        super(1, 1); //variable , objetivo
+        super(2, 1); //n variables totales , n de variables objetivo
     }
 
     @Override
     public void evaluate(Solution sltn) {
+        /* numerar variables de todo el problema */
         double x = EncodingUtils.getReal(sltn.getVariable(0));
-        sltn.setObjective(0, Math.pow(x,2.0));
+        double y = EncodingUtils.getReal(sltn.getVariable(1));
+        
+        //funcion a optimizar
+        double f1 = 0.26*( Math.pow(x,2)+ Math.pow(y,2) ) - 0.48*x*y;
+        
+        sltn.setObjective(0, f1);  //indice de fx objetivo , fx a optimizar
     }
 
     @Override
     public Solution newSolution() {
-        Solution solution = new Solution (1,1);
-        solution.setVariable(0, EncodingUtils.newReal(-1000.0,1000.0));
+        Solution solution = new Solution (2,1); //n variables totales , n de variables objetivo
+        solution.setVariable(0, EncodingUtils.newReal(-10.0,10.0));  // dominio de busqueda
+        solution.setVariable(1, EncodingUtils.newReal(-10.0,10.0));
         return solution;
     }
     
